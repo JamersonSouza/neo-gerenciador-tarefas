e318d301ff8b8f439756c9599c844c01ad0fd90c
JamersonSouza <jamersonsouza@outlook.com>
2020-02-09 12:50:46 -0300

    Inicio do Projeto de Gerenciamento de Tarefas

---
+++ src/main/java/com/br/neo/tarefas/NeoGerenciamentoTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.br.neo.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoGerenciamentoTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoGerenciamentoTarefasApplication.class, args);
+	}
+
+}
