01dc6d3a61e79e76c35d40d256a0fbc4ae249ee1
JamersonSouza <jamersonsouza@outlook.com>
2020-02-09 14:02:37 -0300

    implementacao da classe tarefa

---
+++ src/main/java/com/br/neo/tarefas/model/Tarefa.java
@@ -0,0 +1,69 @@
+package com.br.neo.tarefas.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "gerenciador_tarefas")
+public class Tarefa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "titulo", nullable = false, length = 50)
+	private String titulo;
+	@Column(name = "descricao", nullable = true, length = 100)
+	private String descricao;
+	@Column(name = "data_expiracao", nullable = false)
+	private Date dataExpiracao;
+	@Column(name = "situacao", nullable = false)
+	private Boolean situacao = false;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDataExpiracao() {
+		return dataExpiracao;
+	}
+
+	public void setDataExpiracao(Date dataExpiracao) {
+		this.dataExpiracao = dataExpiracao;
+	}
+
+	public Boolean getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(Boolean situacao) {
+		this.situacao = situacao;
+	}
+
+}
